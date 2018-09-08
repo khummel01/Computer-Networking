5843ea547f61daac30834df63670fc4c6ae4f58b
Katherine Hummel <hummka01@LCubuntu.localdomain>
2018-09-08 14:07:49 -0500

    Cleaned up client code

--- project1/geo_client_tcp.py
+++ project1/geo_client_tcp.py
@@ -9,25 +9,26 @@ HOST = 'localhost'
 PORT = 4300
 
 
-def client(s):
-    '''Main client loop'''
+def client():
     # TODO: Implement client-side tasks
-    country = input()
-    while country != "BYE":
-        s.sendall(country.encode())
-        data = s.recv(1024)        
-        print(data.decode())
-        country = input()
-    s.close()
-    
-
-def main():
-    '''Main function'''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         data = s.recv(1024)
         print(data.decode())
-        client(s)
+        
+        '''Main client loop'''
+        country = input()
+        while country != "BYE":
+            s.sendall(country.encode())
+            data = s.recv(1024)        
+            print(data.decode())
+            country = input()
+        s.close()
+    
+
+def main():
+    '''Main function'''
+    client()
 
 if __name__ == "__main__":
     main()
