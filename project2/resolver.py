73a931ac6b7b1485f6e6033c64b16c2b23b1193f
khummel01 <hummka01@luther.edu>
2018-09-25 16:50:14 -0500

    Converting functions work

--- project2/resolver.py
+++ project2/resolver.py
@@ -37,19 +37,40 @@ def val_to_2_bytes(value: int) -> list:
     '''Split a value into 2 bytes'''
     val_left = value >> 8
     val_right = value & 0xFF
-    byteLst = []
-    byteLst.append(val_left)
-    byteLst.append(val_right)
-    # print(byteLst)
-    return byteLst
+    return [val_left, val_right]
 
 def val_to_n_bytes(value: int, n_bytes: int) -> list:
     '''Split a value into n bytes'''
-    raise NotImplementedError
+    byteLst = []
+    shift = 0
+    # for i in range(n_bytes):
+    #     byteLst.insert(0, (value >> shift))
+    #     shift += 8
+
+    byteLst.insert(0, (value & 0x000000FF)>>0)
+    byteLst.insert(0, (value & 0x0000FF00)>>8)
+    byteLst.insert(0, (value & 0x00FF0000)>>16)
+
+    # byteLst.insert(0, (value & 0xFF000000)>>24)
+    # byteLst.insert(0, (value & 0x000000FF)>>32) # todo: what next?
+
+    print(byteLst)
+    return byteLst
 
 def bytes_to_val(bytes_lst: list) -> int:
     '''Merge 2 bytes into a value'''
-    raise NotImplementedError
+    value = 0
+    shift = 0
+    reshifted = []
+    for i in range(len(bytes_lst)-1,-1,-1):
+        reshifted.append(bytes_lst[i] << shift)
+        shift += 8
+        print(str(bytes_lst[i]), bytes_lst[i] << shift)
+
+    for i in range(len(reshifted)):
+        value += reshifted[i]
+
+    return value
 
 def get_2_bits(bytes_lst: list) -> int:
     '''Extract first two bits of a two-byte sequence'''
@@ -57,7 +78,7 @@ def get_2_bits(bytes_lst: list) -> int:
 
 def get_offset(bytes_lst: list) -> int:
     '''Extract size of the offset from a two-byte sequence'''
-    raise NotImplementedError
+    return ((bytes_lst[0] & 0x3f) << 8) + bytes_lst[1]
 
 def parse_cli_query(filename, q_type, q_domain, q_server=None) -> tuple:
     '''Parse command-line query'''
@@ -68,9 +89,7 @@ def parse_cli_query(filename, q_type, q_domain, q_server=None) -> tuple:
         q_server = PUBLIC_DNS_SERVER[2]
     return (DNS_TYPES[q_type], q_domain.split("."), q_server)
 
-##################################################################################################
 def format_query(q_type: int, q_domain: list) -> bytearray:
-    # todo: where does the q_type go in the bytearray
     transaction_id = random.randint(1, 60000)
     transaction_id_arr = val_to_2_bytes(transaction_id)
     byteArr = bytearray()
@@ -86,22 +105,22 @@ def format_query(q_type: int, q_domain: list) -> bytearray:
     byteArr.append(0)
     byteArr.append(0)
     byteArr.append(0)
-    byteArr.append(len(q_domain[0]))
-    byteArr = byteArr + bytearray(q_domain[0], "utf-8")
-    byteArr.append(len(q_domain[1]))
-    byteArr = byteArr + bytearray(q_domain[1], "utf-8")
+
+    for domain in q_domain:
+        byteArr.append(len(domain))
+        byteArr = byteArr + bytearray(domain, "utf-8")
+
     byteArr.append(0)
 
-    q_domain_2_bytes = val_to_2_bytes(q_type) # todo: in the right spot?
-    byteArr.append(q_domain_2_bytes[0])
-    byteArr.append(q_domain_2_bytes[1])
+    q_type_2_bytes = val_to_2_bytes(q_type)
+    byteArr.append(q_type_2_bytes[0])
+    byteArr.append(q_type_2_bytes[1])
 
     byteArr.append(0)
     byteArr.append(1)
 
-    print(byteArr)
+    # print(byteArr)
     return byteArr
-
 
 def send_request(q_message: bytearray, q_server: str) -> bytes:
     '''Contact the server'''
@@ -110,9 +129,10 @@ def send_request(q_message: bytearray, q_server: str) -> bytes:
     (q_response, _) = client_sckt.recvfrom(2048)
     client_sckt.close()
 
-    print(q_response)
+    # print(q_response)
     return q_response
 
+##################################################################################################
 def parse_response(resp_bytes: bytes):
     '''Parse server response'''
     print(resp_bytes)
@@ -134,6 +154,8 @@ def resolve(query: str) -> None:
     q_type, q_domain, q_server = parse_cli_query(*query[0]) # done
     query_bytes = format_query(q_type, q_domain) # in progress
     response_bytes = send_request(query_bytes, q_server) # done
+    val_to_n_bytes(430430, 3)
+    # bytes_to_val([6, 145, 94])
     # answers = parse_response(response_bytes)
     #print('DNS server used: {}'.format(q_server))
     #for a in answers:
@@ -141,10 +163,10 @@ def resolve(query: str) -> None:
         #print('TTL: {}'.format(a[1]))
         #print('Address: {}'.format(a[2]))
 
-def main(): # *query
+def main(*query): # *query
     '''Main function'''
-    query = [["resolver.py", "A", "luther.edu", "1.1.1.1"]]
-    print(query)
+    # query = [["resolver.py", "A", "luther.edu", "1.1.1.1"]]
+    # print(query)
     if len(query[0]) < 3 or len(query[0]) > 4:
         print('Proper use: python3 resolver.py <type> <domain> <server>')
         exit()
@@ -152,5 +174,5 @@ def main(): # *query
 
 
 if __name__ == '__main__':
-    # main(sys.argv)
-    main()
+    main(sys.argv)
+    # main()
