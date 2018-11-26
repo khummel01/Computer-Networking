685e539d13551a74bf6f840ad4cd331ab3846b63
khummel01 <hummka01@luther.edu>
2018-11-25 20:30:43 -0600

    File descriptor (negative interger) and unpack errors fixed

--- project6/traceroute.py
+++ project6/traceroute.py
@@ -48,8 +48,8 @@ def checksum(pkt: bytes) -> int:
 
 def parse_reply(packet: bytes) -> bool:
     """Parse an ICMP reply"""
-    icmp_data = packet[28:]
     icmp_header = packet[20:28]
+    icmp_data = packet[28:]
 
     pseudo_header = bytearray()
     pseudo_header.extend(icmp_header[0:2])
@@ -57,7 +57,7 @@ def parse_reply(packet: bytes) -> bool:
     pseudo_header.append(0)
     pseudo_header.extend(icmp_header[4:])
 
-    icmp_msg_type, icmp_msg_code, check_sum_rcvd, repl_id, sequence = struct.unpack(....) #todo: fix
+    icmp_msg_type, icmp_msg_code, check_sum_rcvd, repl_id, sequence = struct.unpack("bbHHh", icmp_header)
     check_sum_comptd = checksum(pseudo_header + icmp_data)
     if check_sum_rcvd != socket.htons(check_sum_comptd):
         raise ValueError(f"Incorrect checksum: {check_sum_rcvd}")
@@ -67,11 +67,17 @@ def parse_reply(packet: bytes) -> bool:
         raise ValueError(f"Incorrect type: {icmp_msg_type}. Expected {', '.join([str(x) for x in expected_types])}.")
     return True
 
+#todo: not done
+def send_request(packet: bytes, addr_dst: str, ttl: int) -> socket:
+    """Send an Echo Request"""
+    proto = socket.getprotobyname("icmp")
+    my_icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, proto)
+    my_icmp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack("I", ttl))
+    my_icmp_socket.sendto(packet, (addr_dst, 1))
+    return my_icmp_socket
 
-#todo: NOT DONE!
 def format_request(icmp_type: int, icmp_code: int, req_id: int, seq_num: int) -> bytes:
     """Format an Echo request"""
-
     data = struct.pack("d", time.time())
     chk_header = struct.pack("bbHHh", icmp_type, icmp_code, 0, req_id, seq_num)
     calculated_checksum = socket.htons(checksum(chk_header + data))
@@ -82,14 +88,15 @@ def format_request(icmp_type: int, icmp_code: int, req_id: int, seq_num: int) ->
 
     return header + data
 
-
 def receive_reply(open_socket: socket, timeout: int = 1) -> tuple:
     """Receive an ICMP reply"""
     time_left = timeout
     started_select = time.time()
     what_ready = select.select([open_socket], [], [], time_left)
     how_long_in_select = time.time() - started_select
+    print(what_ready)
     if not what_ready[0]:
+        print("\nin what_ready timeout\n")
         raise TimeoutError("Request timed out")
 
     pkt_rcvd, addr = open_socket.recvfrom(1024)
@@ -100,88 +107,73 @@ def receive_reply(open_socket: socket, timeout: int = 1) -> tuple:
 
     return (pkt_rcvd, addr[0])
 
-#todo: not done
-def send_request(packet: bytes, addr_dst: str, ttl: int) -> socket:
-    """Send an Echo Request"""
-    # line here
-    proto = socket.getprotobyname("icmp")
-    my_icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, proto)
-    my_icmp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack("I", ttl))
-    my_icmp_socket.sendto(packet, (addr_dst, 1))
-    my_icmp_socket.close()
-
-    return my_icmp_socket
-
 def traceroute(hostname: str) -> None:
     """Python traceroute implementation"""
     dest_addr = socket.gethostbyname(hostname)
+    my_id = os.getpid() & 0xFFFF
 
     print(f"Tracing route to {hostname} [{dest_addr}] over a maximum of {MAX_HOPS} hops\n")
 
     received_success = 0
+    parsed_success = 0
+
     for ttl in range(1, MAX_HOPS + 1):
         print(f"{ttl:<5d}", end="")
+        to_error_msg = ""
+        v_error_msg = ""
+        delim = " "
         for att in range(ATTEMPTS):
-            my_id = os.getpid() & 0xFFFF
             packet = format_request(ECHO_REQUEST_TYPE, ECHO_REQUEST_CODE, my_id, att)
             my_icmp_socket = send_request(packet, hostname, ttl)
             try:
+                time_sent = time.time()
                 pkt_rcvd, responder = receive_reply(my_icmp_socket, TIMEOUT)
-                if responder == dest_addr:
-                    continue
+                time_rcvd = time.time()
+                received_success += 1
+                rtt = (time_rcvd - time_sent) * 1000
+                print(f"{rtt:>5.0f} ms", end="")
             except TimeoutError as te:
+                to_error_msg = str(te)
                 print("{:>5s} {:2s}".format("TIME", " "), end="")
+            try:
+                parse_reply(pkt_rcvd)
+                parsed_success += 1
+            except ValueError as ve:
+                v_error_msg = str(ve)
+                print("{:>5s} {:2s}".format("ERR", " "), end="")
+            finally:
+                my_icmp_socket.close()
 
-            parse_reply(pkt_rcvd)
+        if to_error_msg:
+            print(f"{delim:3s} {to_error_msg}")
+        elif v_error_msg:
+            print(f"{delim:3s} {v_error_msg}")
+        else:
+            print(f"{delim:3s} {responder}")
 
+        if responder == dest_addr:
+            break
 
     print("\nTrace complete.")
 
+    #         if v_error_msg:
+    #     if to_error_msg:
+    #         try:
+    #         try:
+    #             continue
+    #         except TimeoutError as te:
+    #      break
+    #     sys.exit(1)
+    # except IndexError:
+    # continue
 
 
 
-                try:
-                try:
-                continue
-                parsed_success += 1
-                print("{:>5s} {:2s}".format("ERR", " "), end="")
-                received_success += 1
-                to_error_msg = str(te)
-                v_error_msg = str(ve)
-            break
-            except TimeoutError as te:
-            header = struct.pack("bbHHh", icmp_header)
-            finally:
-            if to_error_msg:
-            if v_error_msg:
-            print(f"{delim:3s} {responder}")
-            print(f"{delim:3s} {to_error_msg}")
-            print(f"{delim:3s} {v_error_msg}")
-            print(f"{rtt:>5.0f} ms", end="")
-            rtt = (time_rcvd - time_sent) * 1000
-            time_rcvd = time.time()
-            time_sent = time.time()
-            to_error_msg = ""
-            v_error_msg = ""
-        elif v_error_msg:
-        else:
-        if to_error_msg:
-        parsed_success = 0
-        sys.exit(1)
-    # my_icmp_socket.settimeout(timeout)
-    csum = 0
-    csum = 0
-    delim = " "
-    except IndexError:
-    header = struct.pack(
-
-
-
+# # my_icmp_socket.settimeout(timeout)
 def main(args):
-    try:
-        traceroute(args[1])
-    except ValueError as ve:
-        print(f"Usage: {args[0]} <hostname>")
+    traceroute(args[1])
+    # print(f"Usage: {args[0]} <hostname>")
 
-if __name__ == "__main__": main(sys.argv)
+if __name__ == "__main__":
+    main(sys.argv)
 
